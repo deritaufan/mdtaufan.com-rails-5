97ce03c9184b575304153aca41b10c587b9a0878
M Deri Taufan <deritaufan@gmail.com>
2016-08-03 18:37:54 +0200

    Add test to contact me

--- test/integration/contact_me_test.rb
+++ test/integration/contact_me_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class ContactMeTest < ActionDispatch::IntegrationTest
-  # test "invalid contact me information" do
-  # 	get root_path
-  # 	assert_no_difference 'Contact.count' do
-  # 		post contact_path, params: { name: "",
-  # 									 email: "user@invalid",
-  # 									 message: "" }
-  # 	end
-  # 	assert_select 'span.help-block.form-error', count: 3
-  # end
+  test "valid contact me information" do
+  	get root_path
+  	assert_difference 'Contact.count', 1 do
+  		post contact_path, params: { name: "Guest",
+  									 email: "user@mdtaufan.com",
+  									 message: "Test message" }, xhr: true
+  	end
+  	#assert_select 'div.alert.alert-success', "Thank you for your message!"
+  end
 end
