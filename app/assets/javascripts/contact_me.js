1c5058637e9c23098a7afdcde66c2b5e0e56a6fd
M Deri Taufan <deritaufan@gmail.com>
2016-08-16 15:16:27 +0200

    Refactor AJAX for contact me

--- app/assets/javascripts/contact_me.js
+++ app/assets/javascripts/contact_me.js
@@ -1,20 +1,5 @@
 $(document).ready(function() {
     $.validate({
         form: '#contactForm',
-        scrollToTopOnError: false,
-        onSuccess: function($form) {
-            $($form).on("ajax:success",function(e, data, status, xhr){
-                try{
-                    var respone = JSON.parse(xhr.responseText);
-                    if ($("#result").length > 0) {
-                        $("div").remove(".alert, .alert-success");
-                    }
-                    $("#result").append(respone.html);
-                    $($form).trigger("reset");
-                } catch(e) {
-                    console.log(e);
-                }
-            });
-        },
     });
-  });
+});
